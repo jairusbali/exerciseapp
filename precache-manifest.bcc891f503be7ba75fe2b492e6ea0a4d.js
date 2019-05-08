1fb10fc59b9bfb3e50425bc9208333298976c14f
jairus <jairus.bali@gmail.com>
2019-05-08 09:01:11 -0400

    Updates

---
+++ precache-manifest.bcc891f503be7ba75fe2b492e6ea0a4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4145d9092f9192ae4066",
+    "url": "/exerciseapp/static/css/main.13b2098b.chunk.css"
+  },
+  {
+    "revision": "4145d9092f9192ae4066",
+    "url": "/exerciseapp/static/js/main.3e9c79a5.chunk.js"
+  },
+  {
+    "revision": "66cac353717b29bc35fc",
+    "url": "/exerciseapp/static/js/runtime~main.0b096f8b.js"
+  },
+  {
+    "revision": "5469c9d74e43364e9cff",
+    "url": "/exerciseapp/static/js/2.7ad897aa.chunk.js"
+  },
+  {
+    "revision": "f21c61f55f8dfad455cc2fb5021775b3",
+    "url": "/exerciseapp/index.html"
+  }
+];
